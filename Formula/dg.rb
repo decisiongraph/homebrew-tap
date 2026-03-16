da2885cc8fd4de17740b76efabb45c0027484d5d
Onni Hakala <onni@fyff.ee>
2026-03-16 19:11:41 +0200

    chore: update dg to v0.1.0

--- Formula/dg.rb
+++ Formula/dg.rb
@@ -1,33 +1,34 @@
 class Dg < Formula
   desc "Decision Graph - Text-based knowledge graph for company decisions"
   homepage "https://github.com/decisiongraph/dg"
-  version "0.1.3"
+  version "0.1.0"
   license "AGPL-3.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/decisiongraph/dg/releases/download/v0.1.3/dg-aarch64-apple-darwin.tar.gz"
-      sha256 "036d4c04b680775415a6fcfb29ecfb2728d4d4dea03a3f4d9e2a3448e86ed22f"
+      url "https://github.com/decisiongraph/dg/releases/download/v0.1.0/dg-aarch64-apple-darwin.tar.gz"
+      sha256 "fc0179d3013d6f64ba612f038e12637614d8c89d82ced83cff4e148b57df0115"
     end
     on_intel do
-      url "https://github.com/decisiongraph/dg/releases/download/v0.1.3/dg-x86_64-apple-darwin.tar.gz"
-      sha256 "02d5ef0fd50b8645540422dbfb4a0f0f63fd9d606ef7091c3b774cc850e1d0ef"
+      url "https://github.com/decisiongraph/dg/releases/download/v0.1.0/dg-x86_64-apple-darwin.tar.gz"
+      sha256 "ff0713769c6a122e7d0075974cf40ad5cda4c0dc51b0e9247648767d618e668e"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/decisiongraph/dg/releases/download/v0.1.3/dg-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "3d24511b02c553bf3ac37a2f3bbcb3b2258fb338eaf0b7d9f42b36ee2b3d4c3a"
+      url "https://github.com/decisiongraph/dg/releases/download/v0.1.0/dg-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "de1c4ee6ba17517b40dffca4cad9007ecba8961253a8c1a631ae07c9830325ee"
     end
     on_intel do
-      url "https://github.com/decisiongraph/dg/releases/download/v0.1.3/dg-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b7d7327fc189f5740846e80f5c733d65dd10692966ee09ac5bc96782b733fe28"
+      url "https://github.com/decisiongraph/dg/releases/download/v0.1.0/dg-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "fa9d25bf6e6309fcac9cb1906fb7e921ca936fea7378fe61603add0563e30704"
     end
   end
 
   def install
     bin.install "dg"
+    bin.install "dg-mcp"
   end
 
   test do
